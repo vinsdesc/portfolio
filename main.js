6669ed7ea0a35385726939e4ef9f1e62aaa7be40
Vincent <vincent.descamps@proxistore.com>
2023-05-02 16:56:39 +0200

    add custom follow cursor

--- main.js
+++ main.js
@@ -10,6 +10,53 @@ const menuBtn = document.querySelector('#menu-btn');
 const contactLinks = document.querySelectorAll('#contact h1');
 
 
+var cursor = document.querySelector('.cursor'),
+  cursorScale = document.querySelectorAll('.cursor-scale'),
+  mouseX = 0,
+  mouseY = 0
+
+gsap.to({}, 0.016, {
+  repeat: -1,
+
+  onRepeat: function () {
+    gsap.set(cursor, {
+      css: {
+        left: mouseX,
+        top: mouseY
+      }
+    })
+  }
+});
+
+window.addEventListener("mousemove", function (e) {
+  mouseX = e.clientX;
+  mouseY = e.clientY
+});
+
+cursorScale.forEach(link => {
+  link.addEventListener('mouseleave', () => {
+    cursor.classList.remove('grow');
+    cursor.classList.remove('grow-small');
+  });
+  link.addEventListener('mousemove', () => {
+    cursor.classList.add('grow');
+    if (link.classList.contains('small')) {
+      cursor.classList.remove('grow');
+      cursor.classList.add('grow-small');
+    }
+  });
+});
+
+
+
+gsap.utils.toArray(".clickable").forEach(elem => {
+  console.log(elem)
+  let hover = gsap.to(cursor, { scale: 2, color: "blue", duration: .4, paused: true, ease: "power1.inOut" });
+  elem.addEventListener("mouseenter", () => hover.play());
+  elem.addEventListener("mouseleave", () => hover.reverse());
+});
+
+
 menuBtn.addEventListener('click', () => {
   mobileMenu.classList.add('grid');
   mobileMenu.classList.remove('hidden');
